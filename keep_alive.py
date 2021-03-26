3af0a32fce8745e1b6b54c7bb812fa401c6ed1d2
Charles GH <charlesvyelland@rocketmail.com>
2021-03-26 18:09:33 +0000

    Create keep_alive.py

---
+++ keep_alive.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 26 18:09:10 2021
+
+@author: charl
+"""
+
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Hello, I'm still alive"
+
+def run():
+  app.run(host = '0.0.0.0',port=8080)
+
+def keep_alive():
+  t = Thread(target=run)
+  t.start()
